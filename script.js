ff967c9b95967ad10a4e4d82f27f2c1c31a1f1f1
maxklevin35-jpg <maxklevin35@gmail.com>
2026-03-17 01:30:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ function updateClock(){
 
 let now = new Date()
 
-let hours = now.getHours()
-let minutes = now.getMinutes()
-let seconds = now.getSeconds()
+let h = String(now.getHours()).padStart(2,"0")
+let m = String(now.getMinutes()).padStart(2,"0")
+let s = String(now.getSeconds()).padStart(2,"0")
 
-document.getElementById("hours").innerText = hours
-document.getElementById("minutes").innerText = minutes
-document.getElementById("seconds").innerText = seconds
+document.getElementById("hours-top").textContent = h
+document.getElementById("hours-bottom").textContent = h
+
+document.getElementById("minutes-top").textContent = m
+document.getElementById("minutes-bottom").textContent = m
+
+document.getElementById("seconds-top").textContent = s
+document.getElementById("seconds-bottom").textContent = s
 
 }
 
